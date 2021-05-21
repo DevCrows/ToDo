009d7279f925ed6f40a11bd00745e6d919ca876f
Francisco Barrios <francisco.barrios@ifyoucan.com>
2021-05-21 08:44:59 +0700

    update gradle

--- app/src/main/java/com/fjbg/todo/ui/MainActivity.kt
+++ app/src/main/java/com/fjbg/todo/ui/MainActivity.kt
@@ -9,29 +9,29 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    lateinit var binder: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binder = ActivityMainBinding.inflate(layoutInflater)
-        val view = binder.root
-        setContentView(view)
-
-        binder.bottomNavView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.action_home -> {
-                    findNavController(R.id.nav_host_fragment).navigate(R.id.HomeFragment)
-                    true
-                }
-                R.id.action_new_task -> {
-                    findNavController(R.id.nav_host_fragment).navigate(R.id.NewTaskFragment)
-                    true
-                }
-                else -> false
-            }
-        }
-        binder.bottomNavView.setOnNavigationItemReselectedListener { }
-    }
-
+	
+	lateinit var binder: ActivityMainBinding
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binder = ActivityMainBinding.inflate(layoutInflater)
+		val view = binder.root
+		setContentView(view)
+		
+		binder.bottomNavView.setOnNavigationItemSelectedListener { item ->
+			when (item.itemId) {
+				R.id.action_home -> {
+					findNavController(R.id.nav_host_fragment).navigate(R.id.HomeFragment)
+					true
+				}
+				R.id.action_new_task -> {
+					findNavController(R.id.nav_host_fragment).navigate(R.id.NewTaskFragment)
+					true
+				}
+				else -> false
+			}
+		}
+		binder.bottomNavView.setOnNavigationItemReselectedListener { }
+	}
+	
 }
